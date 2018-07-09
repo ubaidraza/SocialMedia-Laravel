cce97cbc0b9152b3b47fb40bec109bb8daf6364b
muhammad ubaid <mubaid900@gmail.com>
2018-07-09 19:18:09 +0500

    registeration and profile controller created

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class ProfileController extends Controller
+{
+    public function profile($username){
+      //$user = User::whereUsername($username)->first();
+      $user = User::where('username',$username)->first();
+     // $user = User::where('username','=',$username)->first();
+        return $user;
+    }
+}
---
+++ app/User.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password','dob','username',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    public function setUsernameAttribute($value){
+        $this->attributes['username'] = ucfirst($value);
+    }
+
+    public function getUsernameAttribute($value){
+        return strtoupper($value);
+    }
+}
